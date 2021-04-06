c6077be17c024c616112698197596c68ebe4ef1a
MarcDonovan1 <marcdonovan@cmail.carleton.ca>
2021-04-06 00:34:47 -0400

    Initial changes to part b

--- Process1.c
+++ Process1.c
@@ -14,54 +14,57 @@ void killChild(int sts)
         perror("Termination failed failed");
         break;
     default:
-        printf("Process 1 has finished: PID = %d\n", id);
+        printf("\nProcess 1 has finished: PID = %d\n", id);
         kill(id, SIGKILL);
         break;
     }
-    
 }
 
-void killParent(){
+void killParent()
+{
     int stat_val;
     pid_t child_pid = wait(&stat_val);
     int id = getpid();
-    printf("Process 2 has finished: PID = %d\n", id);
+    printf("\nProcess 2 has finished: PID = %d\n", id);
     kill(id, SIGKILL);
-
-    
 }
 
 int main()
-{   
+{
     pid_t pid;
     char *message;
     int n;
     int exit_code;
     printf("fork program starting\n");
     pid = fork();
-    
-     int pidPrint = getpid();
-    printf("%d \n", pidPrint);
+
+    int pidPrint = getpid();
     switch (pid)
     {
     case -1:
         perror("fork failed");
         exit(1);
     case 0:
-        //n = 5;
+        n = 1;
         message = "I am Process 1";
         (void)signal(SIGINT, killChild);
         break;
     default:
-        //n = 3;
+        n = 2;
         message = "I am Process 2";
         (void)signal(SIGINT, killParent);
         break;
     }
     while (1)
     {
+
+        int input;
         puts(message);
-        
-        sleep(1);
+        while (n != input)
+        {
+            sleep(1);
+            scanf("%d", &input);
+        }
+        input = 0;
     }
 }
